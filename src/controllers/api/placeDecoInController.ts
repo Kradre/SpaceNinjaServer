afa3d67a77bfb684017ed55ef994129769ead13e
Kradre <kradre@pngamers.org>
2024-10-14 00:19:33 +0700

    Verification of 200 for placeDecoIn

--- src/controllers/api/placeDecoInController.ts
+++ src/controllers/api/placeDecoInController.ts
@@ -1,11 +1,6 @@
 import { RequestHandler } from "express";
-import { Inventory } from "@/src/models/inventoryModels/inventoryModel";
-import { Guild } from "@/src/models/guildModel";
-import { getAccountIdForRequest } from "@/src/services/loginService";
-import { toOid } from "@/src/helpers/inventoryHelpers";
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-const placeDecoInController: RequestHandler = async (req, res) => {
+const placeDecoInController: RequestHandler = (_req, res) => {
     res.sendStatus(200);
 };
 
